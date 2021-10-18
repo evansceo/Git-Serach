4c101bd0ea4dcfa598b448c3836b21ceea2d543e
evansceo <evans.onyiego@student.moringaschool.com>
2021-10-18 23:10:24 +0300

    profile mordified

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -43,4 +43,14 @@ export class ProfileComponent implements OnInit {
       console.log(profile);
       this.profile = profile;
     });
-    
+    this.service.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.any = repos;
+    });
+  }
+
+  ngOnInit() {
+
+  }
+
+}
