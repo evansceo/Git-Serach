b560a973dcca9fc4c23dab2d9f456ba2a71f5e9b
evansceo <evans.onyiego@student.moringaschool.com>
2021-10-18 22:54:15 +0300

    app.module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,7 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { ProfileService } from './services/profile.service';
-import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 
 @NgModule({
