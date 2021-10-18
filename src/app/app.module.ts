82cb9bb24dfdefd3395bdb787e749b2bcc3acbcc
evansceo <evans.onyiego@student.moringaschool.com>
2021-10-18 23:02:58 +0300

    app.module import mordification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    
+    NgbModule
     
   ],
   providers: [ProfileService],
